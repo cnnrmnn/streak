d13424c86133516a4118178bbdf7a582c0be0ebf
cnnrmnn <connor.mann@outlook.com>
2021-02-12 11:01:26 -0500

    Added utilities to parse and encode JSON

--- src/file.lisp
+++ src/file.lisp
@@ -1,10 +1,14 @@
 (defpackage :file
   (:use :common-lisp)
-  (:import-from :yason :with-output
-                       :with-object
-                       :encode-object-elements)
+  (:import-from :yason :encode
+                       :encode-object-elements
+                       :parse
+                       :with-output
+                       :with-object)
   (:export #:*program-dir*
            #:get-streak-file
+           #:parse-json-from-file
+           #:encode-json-dom
            #:write-json-object-elements))
 
 (in-package :file)
@@ -13,6 +17,20 @@
 
 (defun get-streak-file (name)
   (format nil "~A~A.json" *program-dir* name))
+
+(defun parse-json-from-file (namestring)
+  (with-open-file (str namestring
+                       :direction :input
+                       :if-does-not-exist nil)
+    (and str
+         (parse str))))
+
+(defun encode-json-dom (json-dom namestring)
+  (with-open-file (str namestring
+                       :direction :output
+                       :if-exists :supersede
+                       :if-does-not-exist :create)
+    (encode json-dom str)))
 
 (defmacro write-json-object-elements (file &rest elements)
   (let ((str (gensym)))
